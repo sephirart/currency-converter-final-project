7b8f5f5795580750680895459f7faa22b4202d44
Seph <137762594+sephirart@users.noreply.github.com>
2023-10-06 08:46:25 +0200

    Updated functions
    added the convert function and the getting exchange rate one

--- project.py
+++ project.py
@@ -1,11 +1,12 @@
 # -- IDEA: --
 # A currency converter that uses an external API
+# Gets the users input and cleans it up + error management
 #
 
 # -- REQUIREMENTS: --
 # [x] name is project.py
 # [x] main function
-# [] 3 functions minimum
+# [x] 3 functions minimum
 # [] test_project.py file that tests project.py
 # [x] all libraries in requirements.txt, one per line
 
@@ -14,6 +15,7 @@
 # Import libraries
 from requests import get
 from pprint import PrettyPrinter
+import sys
 
 # Currency API and key(expires after a while- to renew)
 BASE_URL = "https://free.currconv.com/"
@@ -39,9 +41,49 @@ def req_currency():
     return results
 
 
+# FUNC2: Printing the currencies
+def print_currency(currencies):
+    for name, currency in currencies:
+        # Get currency name and id
+        curr_name = currency["currencyName"]
+        curr_id = currency["id"]
+        # Not all of them have a symbol in the JSON so give default
+        curr_symbol = currency.get("currencySymbol", "")
+        print(f"{curr_id} - {curr_name} - {curr_symbol}")
+
+
+# FUNC3: Getting the convert rate
+def get_rate(currency1, currency2):
+    # Make correct link to get convert rate
+    endpoint = (
+        f"api/v7/convert?q={currency1}_{currency2}&compact=ultra&apiKey={API_KEY}"
+    )
+    link = BASE_URL + endpoint
+    # Get JSON file
+    response = get(link).json()
+    # return results converted in list
+    exchange_rate = list(response.values())[0]
+    return f"Exchange rate from {currency1} to {currency2} is: {exchange_rate}"
+
+
+# FUNC4: Converting the currencies with convert rate
+def convert(currency1, currency2, amount):
+    get_rate(currency1, currency2)
+
+
 # Main function
 def main():
-    req_currency()
+    # Get currencies -- trying to clean user's input
+    curr1 = input("Please enter currency to be converted (ex. USD): ").upper().strip()
+    curr2 = input("Please enter currency to covert to (ex. CAD): ").upper().strip()
+    amount = input("Please enter amount: ")
+    # Error Management
+    if len(curr1) > 3 or len(curr2) > 3:
+        sys.exit("Invalid currency")
+    # Move on with everything else
+    data = req_currency()
+    print_currency(data)
+    convert(curr1, curr2, amount)
 
 
 if __name__ == "__main__":
